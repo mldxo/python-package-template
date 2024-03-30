a39197b1921791eb26dea2e0a817c1ea79495482
mldxo <miloszmaculewicz@gmail.com>
2024-03-30 19:01:35 +0100

    Add docstring, add version, add dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-# setup.py
+"""Setup file for the python package template"""
+
 from setuptools import setup, find_packages
+from python_package_template.version import __version__ as version
 
 with open("README.md", "r") as file:
     description = file.read()
 
 setup(
     name = "python-package-template",
-    version = "0.1",
+    version = version,
     packages = find_packages(),
-    install_requires = [], # insert dependencies
+    install_requires = [
+        #"your-dependency",
+    ],
     entry_points = {
         "console_scripts" : [
             "python-package-template = python_package_template:main",
